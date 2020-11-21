b1bfc27c02bd5fa7433ea54e199423e3486e2448
Lisa Wagner <lisawagner@Lisas-Air.fritz.box>
2020-11-21 10:54:10 +0100

    finished level 3 solution

--- solutions/Frankenstein_Level3.py
+++ solutions/Frankenstein_Level3.py
@@ -1,29 +1,52 @@
 def create_badge_number():
     '''
-    format xxx-xxx-xxx
-    requirements per block of 3: cross sum is 9, unique digits
+    create a badge number with format xxx-xxx-xxx
+    requirements per block of 3: cross sum is between 9 and 15, the digits are digits
     '''
 
-    def cross_sum(a):
+    def unique_digits(a):                                       # function to check uniqueness of digits in a number
 
-        return 9
+        digits = [int(i) for i in str(a)]
 
-    ints = []
+        print(digits)
+
+        if digits[0] == digits[1]:
+            return False
+        
+        elif digits[0] == digits[2]:
+            return False
+
+        elif digits[1] == digits[0]:
+            return False
+
+        else:
+            return True
+
+    def cross_sum(a):                                           # function to calculate the cross sum
+
+        cross_sum_digits = [int(i) for i in str(a)]
+
+        return sum(cross_sum_digits)
+
+    ints_list = []                                              # list of three digit numbers for the badge number
 
     import random
 
-    for i in range(0,3):
+    while len(ints_list) < 3:                                  # while we have less than 3 items in the list
 
-        x = random.randint(101,998)
+        x = random.randint(101,998)                             # get random 3 digit number
 
-        if cross_sum(x) == 9:
+        if unique_digits(x) == True:                            # check if all digits are unique
 
-            ints.append(x)
+            if cross_sum(x) >= 9 and cross_sum(x) <= 15:        # check if cross sum is 9
 
-    #check requirements
+                ints_list.append(x)                             # add number to ints list
 
-    #badgeNumber = '-'.join(blocks)
+        else:
+            continue
 
-    return(ints)
+    badgeNumber = '-'.join(map(str, ints_list))
+
+    return badgeNumber
 
 print(create_badge_number())
