fb611e67a12e26ea0e7f203fad979fe4aef3ce62
Jess5117 <73069432+Jess5117@users.noreply.github.com>
2020-11-29 13:51:55 +0100

    29.11.2020 2

--- rooms/DerFluchdesPharao.py
+++ rooms/DerFluchdesPharao.py
@@ -10,40 +10,40 @@ class DerFluchdesPharao(EscapeRoom):
     def __init__(self):
         super().__init__()
         self.set_metadata("Alex, Isi, Jessi, Laura", __name__)
-        #self.add_level(self.create_level1())
-        self.add_level(self.create_level2())
+        self.add_level(self.create_level3())
+        self.add_level(self.create_level7())
 
     ### LEVELS ###
     #Level 3 Jessi#
-    #def create_level1(self):
+    def create_level3(self):
 
-        # mythos = random.choice(["Kleopatra liess ihren Mann ermorden,kurz bevor ihre Ehe annulliert werden sollte, um gemeinsam mit Ihrem Sohn an die Macht zu kommen!.", 
-        # "Man sagt, dass Hieroglyphen soviel wie in Stein gravieren heisst. Die Zeichen wurden sogar vielfach farbig geschrieben und sind immer eine Bildreihe!", 
-        # "Nicht nur Pharaonen hatten damals so viel Macht, auch Lokalkönige waren sehr mächtig. So waren sie zuständig für all die Kleinstaaten! Beide Arten von Regenten waren allerdings sehr streng beim Aussieben von Sand!",
-        # "Die goettliche Legitimation beginnt mit der rituellen Aktivierung der Göttlichkeit. Das ist etwa vergleichbar mit einer Routineuntersuchung!" ,
-        # "Vorgänger der fünfkantigen Pyramide ist ein Mstabas, ein einstufiger und flacher Bau. Auch sie hatten bereits Kammern, die tief ins Erdreich verlagert wurden." ,
-        # "Pyramiden dienen als Begräbnisstätten, deren Steine sind aus Kalkstein. Die Bauwerke haben unter anderem Stollen die von Schächten abzweigen. Es gibt eine Pyramide, die während der Bauzeit fünfmal erweitert wurde! Die meisten Pyramiden sind viereckig ",
-        # "Cheops, der boese Pharao, zwang alle Untertanen seines Reviers beim Pyramidenbau zu helfen."  ])
+        mythos = random.choice(["Kleopatra liess ihren Mann ermorden,kurz bevor ihre Ehe annulliert werden sollte, um gemeinsam mit Ihrem Sohn an die Macht zu kommen!.", 
+        "Man sagt, dass Hieroglyphen soviel wie in Stein gravieren heisst. Die Zeichen wurden sogar vielfach farbig geschrieben und sind immer eine Bildreihe!", 
+        "Nicht nur Pharaonen hatten damals so viel Macht, auch Lokalkönige waren sehr mächtig. So waren sie zuständig für all die Kleinstaaten! Beide Arten von Regenten waren allerdings sehr streng beim Aussieben von Sand!",
+        "Die goettliche Legitimation beginnt mit der rituellen Aktivierung der Göttlichkeit. Das ist etwa vergleichbar mit einer Routineuntersuchung!" ,
+        "Vorgänger der fünfkantigen Pyramide ist ein Mstabas, ein einstufiger und flacher Bau. Auch sie hatten bereits Kammern, die tief ins Erdreich verlagert wurden." ,
+        "Pyramiden dienen als Begräbnisstätten, deren Steine sind aus Kalkstein. Die Bauwerke haben unter anderem Stollen die von Schächten abzweigen. Es gibt eine Pyramide, die während der Bauzeit fünfmal erweitert wurde! Die meisten Pyramiden sind viereckig ",
+        "Cheops, der boese Pharao, zwang alle Untertanen seines Reviers beim Pyramidenbau zu helfen."  ])
 
-        # zahl_in_worten = ["null", "eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn", "elf"]
-        # code = [0,1,2,3,4,5,6,7,8,9,10,11]
+        zahl_in_worten = ["null", "eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn", "elf"]
+        code = [0,1,2,3,4,5,6,7,8,9,10,11]
                 
-        # task_messages = [ " Du schaust dich im Raum um und entdeckst einen an die Wand geschriebenen Satz!",
-        #     "<i>Denke daran mein Freund Kleopatra nimmts immer <b>wörtlich</b> und sie liebt aufsteigende Reihenfolgen! </i>",
-        #     "<b>"+mythos+"</b>",
-        #     "<br></br>",
-        #     "In einer anderen Ecke siehst du folgende Listen: ",
-        #     zahl_in_worten,
-        #     code
-        #     ]
-        # hints = [
-        #     "1. Wenn du einen Mythos gefunden hast, dann lies ihn genau durch. Ganz genau.",
+        task_messages = [ " Du schaust dich im Raum um und entdeckst einen an die Wand geschriebenen Satz!",
+            "<i>Denke daran mein Freund Kleopatra nimmts immer <b>wörtlich</b> und sie liebt aufsteigende Reihenfolgen! </i>",
+            "<b>"+mythos+"</b>",
+            "<br></br>",
+            "In einer anderen Ecke siehst du folgende auf<b>LIST</b>ung: ",
+            zahl_in_worten,
+            code
+            ]
+        hints = [
+            "1. Wenn du einen Mythos gefunden hast, dann lies ihn genau durch. Ganz genau.",
             
-        # ]
-        # return {"task_messages": task_messages, "hints": hints, "solution_function": self.mythos, "data": mythos}
+        ]
+        return {"task_messages": task_messages, "hints": hints, "solution_function": self.mythos, "data": mythos}
     
     #Level 7 Jess #
-    def create_level2(self):
+    def create_level7(self):
 
         bauplan = [
         "                /\ ",
@@ -98,41 +98,37 @@ class DerFluchdesPharao(EscapeRoom):
 
     ### SOLUTIONS ###
 
-    #def mythos(self, mythos):
-        # zahl_in_worten = ["null", "eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn", "elf"]
-        # code = [0,1,2,3,4,5,6,7,8,9,10,11]
+    def mythos(self, mythos):
+        zahl_in_worten = ["null", "eins", "zwei", "drei", "vier", "fünf", "sechs", "sieben", "acht", "neun", "zehn", "elf"]
+        code = [0,1,2,3,4,5,6,7,8,9,10,11]
 
-        # for word in zahl_in_worten:
-        #         if (word in mythos):
-        #             print(code[zahl_in_worten.index(word)])
+        for word in zahl_in_worten:
+                if (word in mythos):
+                    print(code[zahl_in_worten.index(word)])
 
-        # return str(135)
+        return str(135)
 
 
     def bauplan(self,rohbau):
 
-        
-        #Datei einlesen
-        textfile = open('pyramide.txt', 'r')
-        lines = textfile.readlines()
-        letter = "".join(lines)
-        #print(letter)
-        
+        #Liste reinkopieren aus txt file      
+        lines = ['                /\\ ', '     /[][][]M[][][][][][][][\\ ', '       /[][]H[][][][][][][\\ ', '              /[][[\\ ', '   /[][][][][][][][]N[][][][][\\ ', '        /[][][][]C[][][][\\ ', '               /][\\ ', '      /[]A[][][][][][][][][\\ ', '         /[][][]N[][][][\\ ', '    /[]U[][][][][][][][][][][\\ ', '           /[][][]T[][\\ ', '            /[][][]U[\\ ', '          /[][][]A[][][\\ ', '             /[][]T[\\ ']
+
         #zählen der Steine [] & sortieren um es als richtige Pyramide auszugeben
         for i in range(len(lines)):
-        
-            letter.count("[]") 
+
+            lines.count("[]") 
             lines.sort()
-            
-        letter="".join(lines)
-        print(letter)
+
+        print("\n ")
+
+        for i in range(len(lines)):
+            print(lines[i])
 
         #Zeilen zu einem Eintrag zusammenführen und alles außer Buchstaben entfernen    
         letterneu = []
-        print("\n ")
-        
         for l in range(len(lines)):
-            #Wortliste zusammenführen zu einem String
+            #Wortliste zusammenführen zu einem String und alles außer Buchstaben löschen
             letter = "".join(lines[l])
             letter = letter.replace("[", "")
             letter = letter.replace("]", "")
@@ -141,66 +137,21 @@ class DerFluchdesPharao(EscapeRoom):
             letter = letter.replace("\n", "")
             letter = letter.replace("\\", "")
             letterneu +=letter
-
-        letterneu = "".join(letterneu)
-        
-        print("Lösungswort: " + letterneu + "\n ") #Ausgabe N
-
-        code = 0
-        #Berechnen des ACSII Code für die einzelnen Buchstaben & alle zusammenrechnen = Code 840
-        for i in range(len(letterneu)):   
-        
-            zahl = ord(letterneu[i])
-            code +=zahl
-        print("Lösungscode: " + str(code))
-
-        textfile.close()
-
-        loesungswort = "TUTANCHAMUN"
-
-        return loesungswort
-        
-        """ #Durcheinander geratener Bauplan zusammen bauen 
-        for i in range(len(rohbau)):
-            print(rohbau[i])
-        
-        #zählen der Steine [] & sortieren um es als richtige Pyramide auszugeben
-        for i in range(len(rohbau)):
-        
-            rohbau.count("[]") 
-            rohbau.sort()
-        
-        print("\n ")
-        
-        for i in range(len(rohbau)):
-            print(rohbau[i])
-
-        #Zeilen zu einem Eintrag zusammenführen und alles außer Buchstaben entfernen    
-        letterneu = []
-        for l in range(len(rohbau)):
-            #Wortliste zusammenführen zu einem String und alles außer Buchstaben löschen
-            letter = "".join(rohbau[l])
-            letter = letter.replace("[", "")
-            letter = letter.replace("]", "")
-            letter = letter.replace(" ", "")
-            letter = letter.replace("/", "")
-            letter = letter.replace("\n", "")
-            letter = letter.replace("\\", "")
-            letterneu +=letter
             
         letterneu = "".join(letterneu)
-        
+
         print("\nLösungswort: " + letterneu + "\n ") #Ausgabe 
 
         code = 0
         #Berechnen des ACSII Code für die einzelnen Buchstaben & alle zusammenrechnen = Code 840
         for i in range(len(letterneu)):   
-        
+            
             zahl = ord(letterneu[i])
             code +=zahl
         print("Lösungscode: " + str(code))
 
 
         loesungswort = "TUTANCHAMUN"
+        return loesungswort
+    
 
-        return loesungswort """
--- solutions/bauplan_richtig.py
+++ solutions/bauplan_richtig.py
@@ -1,68 +1,45 @@
-# rei =("         /[][][][]C[][][][\ 
-#                /][\ 
-#              /[][]T[\ 
-#             /[][][]U[\ 
-#       /[]A[][][][][][][][][\ 
-#            /[][][]T[][\ 
-#               /[][[\ 
-#      /[][][]M[][][][][][][][\ 
-#        /[][]H[][][][][][][\ 
-#     /[]U[][][][][][][][][][][\ 
-#           /[][][]A[][][\ 
-#                 /\ 
-#          /[][][]N[][][][\ 
-#    /[][][][][][][][]N[][][][][\ ""
-import requests
-
-# lines = rei.split(",")
-re=()
-r = requests.get("https://raw.githubusercontent.com/alex2101998/pythonescaperoom/Jess/static/pyramide.txt")
-re = r
-print(r.content)
-print(re)
-print(type(r))
-
-lines = r.split("\n")
-print(lines)
-
-#zählen der Steine [] & sortieren um es als richtige Pyramide auszugeben
-for i in range(len(lines)):
-
-    lines.count("[]") 
-    lines.sort()
-
-print("\n ")
-
-for i in range(len(lines)):
-    print(lines[i])
-
-#Zeilen zu einem Eintrag zusammenführen und alles außer Buchstaben entfernen    
-letterneu = []
-for l in range(len(lines)):
-    #Wortliste zusammenführen zu einem String und alles außer Buchstaben löschen
-    letter = "".join(lines[l])
-    letter = letter.replace("[", "")
-    letter = letter.replace("]", "")
-    letter = letter.replace(" ", "")
-    letter = letter.replace("/", "")
-    letter = letter.replace("\n", "")
-    letter = letter.replace("\\", "")
-    letterneu +=letter
-    
-letterneu = "".join(letterneu)
-
-print("\nLösungswort: " + letterneu + "\n ") #Ausgabe 
-
-code = 0
-#Berechnen des ACSII Code für die einzelnen Buchstaben & alle zusammenrechnen = Code 840
-for i in range(len(letterneu)):   
-    
-    zahl = ord(letterneu[i])
-    code +=zahl
-print("Lösungscode: " + str(code))
+def run(rohbau):
+    lines = ['                /\\ ', '     /[][][]M[][][][][][][][\\ ', '       /[][]H[][][][][][][\\ ', '              /[][[\\ ', '   /[][][][][][][][]N[][][][][\\ ', '        /[][][][]C[][][][\\ ', '               /][\\ ', '      /[]A[][][][][][][][][\\ ', '         /[][][]N[][][][\\ ', '    /[]U[][][][][][][][][][][\\ ', '           /[][][]T[][\\ ', '            /[][][]U[\\ ', '          /[][][]A[][][\\ ', '             /[][]T[\\ ']
 
 
-loesungswort = "TUTANCHAMUN"
+    #zählen der Steine [] & sortieren um es als richtige Pyramide auszugeben
+    for i in range(len(lines)):
 
-    
+        lines.count("[]") 
+        lines.sort()
+
+    print("\n ")
+
+    for i in range(len(lines)):
+        print(lines[i])
+
+    #Zeilen zu einem Eintrag zusammenführen und alles außer Buchstaben entfernen    
+    letterneu = []
+    for l in range(len(lines)):
+        #Wortliste zusammenführen zu einem String und alles außer Buchstaben löschen
+        letter = "".join(lines[l])
+        letter = letter.replace("[", "")
+        letter = letter.replace("]", "")
+        letter = letter.replace(" ", "")
+        letter = letter.replace("/", "")
+        letter = letter.replace("\n", "")
+        letter = letter.replace("\\", "")
+        letterneu +=letter
+        
+    letterneu = "".join(letterneu)
+
+    print("\nLösungswort: " + letterneu + "\n ") #Ausgabe 
+
+    code = 0
+    #Berechnen des ACSII Code für die einzelnen Buchstaben & alle zusammenrechnen = Code 840
+    for i in range(len(letterneu)):   
+        
+        zahl = ord(letterneu[i])
+        code +=zahl
+    print("Lösungscode: " + str(code))
+
+
+    loesungswort = "TUTANCHAMUN"
+
+    return loesungswort   
 
