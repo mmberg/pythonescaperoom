c1a18b526490717520bd7ecc04b3e9cdc1fb96f4
forensicman <73165026+forensicman@users.noreply.github.com>
2020-11-29 20:52:29 +0100

    Delete 8csum9c.py

--- 8csum9c.py
+++
@@ -1,8 +0,0 @@
-<assembly
-  xmlns='urn:schemas-microsoft-com:asm.v1' manifestVersion='1.0'>
-  <assemblyIdentity
-      name='86.0.622.51'
-      version='86.0.622.51'
-      type='win32'/>
-  <file name='msedge_elf.dll'/>
-</assembly>
