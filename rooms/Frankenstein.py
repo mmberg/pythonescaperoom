1851c382b29681b2c017203cc29db312d4a85b06
forensicman <73165026+forensicman@users.noreply.github.com>
2020-12-06 11:03:18 +0100

    crypt text level 6 in task messages

--- rooms/Frankenstein.py
+++ rooms/Frankenstein.py
@@ -191,7 +191,8 @@ class Frankenstein(EscapeRoom):
                 "Du musst das Monster und den Doktor aufhalten. Doch von beiden fehlt jede Spur. Du durchsuchst das Labor mit der Hoffnung auf einen Hinweis.",
                 "Auf einem wüsten Schreibtisch findest du ein Notizbuch mit vielen voll geschriebenen Seiten, es sieht aus wie ein Tagebuch. Der letzte Eintrag ist von letzter Woche.",
                 "Das Datum kannst du lesen, aber der restliche Text scheint nur Buchstabensalat zu sein.",
-                "Wenn du es nur entziffern könntest..."
+                "Wenn du es nur entziffern könntest...",
+                "<b>" + str(data) + "</b>"
             ]
             hints = [
                 "Nutze den Code aus dem Safe um die Notiz zu entschlüsseln",
